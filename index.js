575c8da19abfd161a1093fe4d3ee32a5ce08c3a5
Simon Taylor <nizmox@gmail.com>
2015-08-23 12:06:58 +1000

    add build query function and refactor

--- index.js
+++ index.js
@@ -23,9 +23,9 @@ var REQUIRED_FIELDS = ["modelArray", "storeWhere", "arrayPop", "mongooseModel", 
 
 function reversePopulate(opts, cb) {
 
-	//check required fields have been provided
-	try { checkRequired(REQUIRED_FIELDS, opts) }
-	catch (ex) { return cb(ex) }
+	// Check required fields have been provided
+	try { checkRequired(REQUIRED_FIELDS, opts); }
+	catch (ex) { return cb(ex); }
 
 	// If empty array passed, exit!
 	if (!opts.modelArray.length) return cb(null, opts.modelArray);
@@ -35,102 +35,93 @@ function reversePopulate(opts, cb) {
 
 	var popResult = populateResult.bind(this, opts.storeWhere, opts.arrayPop);
 
-	// build the query string
-	var query = opts.filters || {};
-	var ids = _.pluck(opts.modelArray, "_id");
-	query[opts.idField] = { $in: ids };
-
-	// Set query select() parameter
-	var select = getSelectString(opts.select, opts.idField)
-
-	// Set query populate() parameter or if it's null, return empty string
-	var populate = opts.populate || '';
-
-	// Set query limit() parameter or if it's null, return empty string
-	var limit = opts.limit || '';
-
-	// Set query sort() parameter or if it's null, return empty string
-	var sort = opts.sort || '';
+	var query = buildQuery(opts);
 
 	// Do the query
-	opts.mongooseModel
-		.find(query)
-		.select(select)
-		.populate(populate)
-		.limit(limit)
-		.sort(sort)
-		.exec(function(err, results) {
+	query.exec(function(err, results) {
+		// If there is an error, callback with error
+		if (err) return cb(err);
 
-			// If there is an error, callback with error
-			if (err)
-				return cb(err)
+		// Map over results (models to be populated)
+		results.forEach(function(result) {
 
-			// Map over results (models to be populated)
-			results.forEach(function(result) {
+			// Check if the ID field is an array
+			var isArray = !isNaN(result[opts.idField].length);
 
-				// Check if the ID field is an array
-				var isArray = !isNaN(result[opts.idField].length);
-
-				// If the idField is an array, map through this
-				if (isArray) {
-					result[opts.idField].map(function(resultId) {
-						var match = modelIndex[resultId];
-						// If match found, populate the result inside the match
-						if (match) popResult(match, result)
-					});
-
-				// Id field is not an array
-				} else {
-					// So just add the result to the model
-					var matchId = result[opts.idField]
-					var match = modelIndex[matchId]
-
+			// If the idField is an array, map through this
+			if (isArray) {
+				result[opts.idField].map(function(resultId) {
+					var match = modelIndex[resultId];
 					// If match found, populate the result inside the match
-					if (match) popResult(match, result)
-				}
-			});
+					if (match) popResult(match, result);
+				});
+
+			// Id field is not an array
+			} else {
+				// So just add the result to the model
+				var matchId = result[opts.idField];
+				var match = modelIndex[matchId];
+
+				// If match found, populate the result inside the match
+				if (match) popResult(match, result);
+			}
+		});
 
 		// Callback with passed modelArray
-		cb(null, opts.modelArray)
+		cb(null, opts.modelArray);
 	});
 }
 
 module.exports = reversePopulate;
 
-// Populate the result against the match
-function populateResult(storeWhere, arrayPop, match, result) {
+// Check all mandatory fields have been provided
+function checkRequired(required, opts) {
+	var msg = "Missing mandatory field ";
+	required.forEach(function(fieldName) {
+		if (opts[fieldName] == null) throw new Error(msg + fieldName);
+	});
+}
 
-	// If this is a one to many relationship
-	if (arrayPop) {
+// Build the query string with user provided options
+function buildQuery(opts) {
+	var conditions = opts.filters || {};
+	var ids = _.pluck(opts.modelArray, "_id");
+	conditions[opts.idField] = { $in: ids };
 
-		// Check if array exists, if not create one
-		if (typeof match[storeWhere] === "undefined") match[storeWhere] = [];
+	var query = opts.mongooseModel.find(conditions);
 
-		// Push the result into the array
-		match[storeWhere].push(result);
-
-	// This is a one to one relationship
-	} else {
-
-		// Save the results
-		match[storeWhere] = result;
+	// Set query select() parameter
+	if (opts.select) {
+		var select = getSelectString(opts.select, opts.idField);
+		query = query.select(select);
 	}
-};
 
-//ensure the select option always includes the required id field to populate the relationship
+	if (opts.populate) query = query.populate(opts.populate);
+	if (opts.limit) query = query.limit(opts.limit);
+	if (opts.sort) query = query.sort(opts.sort);
+	return query;
+}
+
+// Ensure the select option always includes the required id field to populate the relationship
 function getSelectString(selectStr, requiredId) {
-	if (!selectStr) return "";
-
 	var selected = selectStr.split(" ");
 	var idIncluded = !!~selected.indexOf(requiredId);
 	if (!idIncluded) return selectStr + " " + requiredId;
 	return selectStr;
 }
 
-// Check all mandatory fields have been provided
-function checkRequired(required, opts) {
-	var msg = "Missing mandatory field "
-	required.forEach(function(fieldName) {
-		if (opts[fieldName] == null) throw new Error(msg + fieldName);
-	});
+// Populate the result against the match
+function populateResult(storeWhere, arrayPop, match, result) {
+	// If this is a one to many relationship
+	if (arrayPop) {
+		// Check if array exists, if not create one
+		if (typeof match[storeWhere] === "undefined") match[storeWhere] = [];
+		// Push the result into the array
+		match[storeWhere].push(result);
+	// This is a one to one relationship
+	} else {
+		// Save the results
+		match[storeWhere] = result;
+	}
 }
+
